c74568e8b1586a8f111a81abbf294137e1cf7fca
Hjordten <andr314e@stud.kea.dk>
2022-12-22 14:29:33 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main (String[] args){
+
+    }
+
+    //Numeric types
+    // - Intergers and real numbers.
+
+    //String
+    // - Word or words inside a program.
+
+    //Boolean
+    // - True or false.
+
+    //Variable naming.
+    // - Must NOT start with a digit.
+    // - Variables may not contain punctuations like comma, period, or backslash/forwards lash
+    // - Variables must not contain spaces.
+    // - Variables cannot be reserved words like void, class, or this.
+    // - Variables are case sensitive.
+
+}
